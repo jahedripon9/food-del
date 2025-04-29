044631b076513bbe17fe619a3c43efccbee197b3
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-04-29 13:03:47 +0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import { connectDB } from './config/db.js';
+import foodRoute from './routers/foodRoute.js';
+import userRouter from './routers/userRoute.js';
+import cartRouter from './routers/cartRoute.js';
+import orderRouter from './routers/orderRoute.js';
+
+// app config
+const app = express();
+const port = 4000
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// db connection
+connectDB();
+
+// API endpoints
+app.use("/api/food", foodRoute);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter)
+
+// hello
+app.get('/', (req, res) => {
+    res.send("API Working")
+})
+
+
+app.listen(port, () => console.log('Server Started on PORT:' + port))
+
