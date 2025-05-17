a851526134a7ac8ec808f52dd69a0da56ae14236
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-05-17 21:05:42 +0600

    update

--- backend/routers/foodRoute.js
+++ backend/routers/foodRoute.js
@@ -1,23 +1,18 @@
 import express from 'express';
 import { addFood, listFood, removeFood } from '../controllers/foodController.js';
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../config/cloudinary.js';
+import authMiddleware from '../middleware/auth.js';
+import upload from '../middleware/multer.js';
+
 
 const foodRoute = express.Router();
 
-// ✅ Use Cloudinary for image storage
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-        folder: 'food_images',
-        allowed_formats: ['jpg', 'png', 'jpeg'],
-    },
-});
 
-const upload = multer({ storage });
 
-foodRoute.post('/add', upload.single('image'), addFood);
+
+
+foodRoute.post('/add', authMiddleware, upload.fields([
+    { name: 'image1', maxCount: 1 }
+]), addFood);
 foodRoute.get('/list', listFood);
 foodRoute.post('/remove', removeFood);
 
